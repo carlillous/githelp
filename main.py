87b5de7f9c6ef93d98d7cb9fb043af5db434fb71
Dani Quinteiro <danqdon@gmail.com>
2024-05-11 12:23:25 +0100

    Complete json with keyword and language

--- main.py
+++ main.py
@@ -7,18 +7,26 @@ if __name__ == "__main__":
     token = "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"
     graphql_client = GraphQLClient(token)
 
-    # Cliente Mongo con autenticacion
+    # Cliente Mongo con autenticación
     mongo_username = "root"
     mongo_password = "admin"
     mongo_client = MongoDBClient("github_data", "repositories", username=mongo_username, password=mongo_password)
-    # Ejecutar consulta y almacenar en MongoDB
-    try:
-        # Realiza la consulta utilizando parámetros personalizados
-        result = graphql_client.search_repositories(keyword="chatgpt", language="Python", stars=10000, first=5)
-        print(json.dumps(result, indent=4))
 
-        # Inserta los resultados en MongoDB
-        mongo_client.insert_elements(result)
+    # Definición de keywords y lenguajes
+    keywords = ["chatgpt", "databases", "scrum", "kaggle", "machine learning", "neural network"]
+    languages = ["Python", "Java", "C", "C++", "Javascript"]
 
-    except Exception as e:
-        print(e)
+    # Ejecución de consultas y almacenamiento en MongoDB
+    for keyword in keywords:
+        for language in languages:
+            print("Searching for", keyword, "and language", language)
+            try:
+                # Realiza la consulta utilizando parámetros personalizados
+                result = graphql_client.search_repositories(keyword=keyword, language=language, stars=1000, first=5)
+                print(json.dumps(result, indent=4))
+
+                # Inserta los resultados en MongoDB, añadiendo keyword y language
+                mongo_client.insert_elements(result, keyword, language)
+
+            except Exception as e:
+                print(e)
--- mongodb/mongo_client.py
+++ mongodb/mongo_client.py
@@ -1,6 +1,5 @@
 import pymongo
 
-
 class MongoDBClient:
     def __init__(self, db, col, username=None, password=None):
         if username and password:
@@ -11,17 +10,20 @@ class MongoDBClient:
         self.database = self.client[db]
         self.collection = self.database[col]
 
-    def insert_elements(self, data_json):
-        """Inserta elementos en la colección desde un JSON."""
+    def insert_elements(self, data_json, keyword, language):
+        """Inserta elementos en la colección desde un JSON, añadiendo información sobre la keyword y language."""
         try:
             documents = [
                 {
                     "name": edge["node"]["name"],
                     "owner": edge["node"]["owner"]["login"],
-                    "stargazers": edge["node"]["stargazers"]["totalCount"]
+                    "stargazers": edge["node"]["stargazers"]["totalCount"],
+                    "keyword": keyword,  # Añade el keyword al documento
+                    "language": language  # Añade el lenguaje al documento
                 }
                 for edge in data_json["data"]["search"]["edges"]
             ]
             self.collection.insert_many(documents)
         except Exception as e:
             print(f"Error al insertar elementos: {e}")
+
