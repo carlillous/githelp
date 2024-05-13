d592873d571813e2fbdb685875681ad5cad22a8b
Carlos <carlos.mendoza103@alu.ulpgc.es>
2024-05-13 21:01:10 +0100

    Connection fixed (still not working).

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
-from pymongo import MongoClient
-from neo4j import GraphDatabase
+from mongodb.mongo_client import MongoDBClient
+from neo4j_integration.neo4j_connection import Neo4JConnection
 
 # Aquí puedes escribir el código para tu aplicación
 st.title('GitHelp')
 
-mongo_client = MongoClient("mongodb://usuario:contraseña@localhost:27017/base_de_datos")
-neo4j_driver = GraphDatabase.driver("bolt://localhost:7687", auth=("usuario", "contraseña"))
+mongo_client = MongoDBClient("github_data", "repositories", username="carlosmenegg", password="agcnc7SuS7f9BPou")
+neo4j_driver = Neo4JConnection("neo4j+ssc://761fb7e4.databases.neo4j.io:", "neo4j", "bUamNfpq4UTlaV1A15xaDf0Tv0oebuQYOKhtX5O068Q")
 
 mongo_query = st.text_area("Consulta para MongoDB")
 neo4j_query = st.text_area("Consulta para Neo4j")
@@ -16,7 +16,7 @@ if st.button("Ejecutar consultas"):
         st.warning("Por favor ingresa al menos una consulta.")
     else:
         if mongo_query.strip() != "":
-            with mongo_client.start_session() as session:
+            with mongo_client.client.start_session() as session:
                 db = mongo_client.base_de_datos
                 collection = db.mi_coleccion
                 results_mongo = collection.find(mongo_query.strip())
